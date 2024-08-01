da75065cc6fabe3fd229183b808daa087b562676
Henrique <hesr.ribeiro@gmail.com>
2024-08-01 20:59:31 -0300

    adiciona máscara e func format

--- ex003.py
+++ ex003.py
@@ -1,4 +1,5 @@
 num1 =int(input('Primeiro número: '))
 num2 = int(input('Segundo número: '))
 soma = num1 + num2
-print('A soma de', num1, 'e', num2, 'é: ', soma)
+# print('A soma de', num1, 'e', num2, 'é: ', soma)
+print('A soma entre {} e {} é {}'.format(num1, num2, soma))
