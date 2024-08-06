ad791d659607a750e8f6a409dd78eebc585b2380
Henrique <henrique.ribeiro@hardworkmedicina.com.br>
2024-08-06 10:59:10 -0300

    exercicio 5

---
+++ ex005.py
@@ -0,0 +1,9 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+sum = n1 + n2
+mult = n1 * n2
+div = n1 / n2
+ex = n1 ** n2
+print('A soma de {} e {} é {}'.format(n1, n2, sum))
+print('A multiplicação de {} e {} é {}'.format(n1, n2, mult))
+print('A divisão de {} e {} é {}'.format(n1, n2, div))
